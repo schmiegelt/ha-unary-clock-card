9ea6aa5f65f18ed8d8e9308e7225e64be6125688
Philip Schmiegelt <philip@schmiegelt.it>
2025-01-31 21:59:44 +0000

    Add config files

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import  typescript  from 'rollup-plugin-typescript2';
+import  nodeResolve  from '@rollup/plugin-node-resolve';
+import  commonjs  from '@rollup/plugin-commonjs';
+import  terser from '@rollup/plugin-terser';
+import  serve from 'rollup-plugin-serve';
+
+const dev = process.env.ROLLUP_WATCH;
+
+const serveopts = {
+  contentBase: ['./dist'],
+  host: '0.0.0.0',
+  port: 5000,
+  allowCrossOrigin: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
+const plugins = [
+  typescript(),
+  commonjs(),
+  nodeResolve(),
+  dev && serve(serveopts),
+  !dev && terser(),
+];
+
+export default [
+  {
+    input: 'src/HaUnaryClockCard.ts',
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [...plugins],
+  },
+];
